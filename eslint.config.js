f0d7ef3563d913d02b568434fbf7ef0a880f31ff
81ckr0s3 <blackrose@yoruverse.com>
2025-02-24 19:35:42 +0100

    Added new project

--- eslint.config.js
+++ eslint.config.js
@@ -1,17 +1,19 @@
-import globals from "globals";
-import pluginJs from "@eslint/js";
-import tseslint from "typescript-eslint";
+import globals from 'globals';
+import pluginJs from '@eslint/js';
+import tseslint from 'typescript-eslint';
 
 
 /** @type {import('eslint').Linter.Config[]} */
 export default [
-  { files: ["**/*.{js,mjs,cjs,ts,astro}"] },
+  { files: ['**/*.{js,mjs,cjs,ts}'] },
   { languageOptions: { globals: { ...globals.browser, ...globals.node } } },
   pluginJs.configs.recommended,
   ...tseslint.configs.recommended,
   {
-    indent: ["warn", 4],
-    semi: ["warn", "always"],
-    quotes: ["warn", "single"],
+    rules: {
+      indent: ['warn', 4],
+      semi: ['warn', 'always'],
+      quotes: ['warn', 'single'],
+    }
   }
 ];
